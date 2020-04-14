5c7f9d4626b59fce8f3b300e8485380fe64c02c4
faycel <debian2007.faical@gmail.com>
2020-04-14 22:15:05 +0100

    added liste of questions with filter control

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,94 +1,119 @@
 import React, { Component } from "react";
-import CoolTabs from "react-cool-tabs";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row, ListGroup } from "react-bootstrap";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
-import {getInitialData} from '../actions/shared'
+import { getInitialData } from "../actions/shared";
+import { Card, Nav } from "react-bootstrap";
 
-class Question extends Component{
-  render(){
-    return(
-      <div>{this.props.name}</div>
-    )
-  }
-}
-
-
-class Content1 extends Component {
+class Question extends Component {
   render() {
-    const questionsList = Object.values(this.props.questions)
+    const question = this.props;
     return (
-      <div style={this.props.style}>
-        <ul>
-         {questionsList.filter(question =>
-          (question.optionOne.votes.indexOf("sarahedo")>-1)||
-          (question.optionTwo.votes.indexOf("sarahedo")>-1)).map(question => <li>{question.author}</li>)}
-        </ul>
-        <Question name={"Nouar"} />     
-      
-      </div>
-
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px160" />
+        <Card.Body>
+          <Card.Title>Card title</Card.Title>
+          <Card.Text>
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </Card.Footer>
+      </Card>
     );
   }
 }
-class Content2 extends Component {
-  render() {
-    const questionsList = Object.values(this.props.questions)
 
+class QuestionList extends Component {
+  render() {
+    const questionsList = Object.values(this.props.questions);
     return (
-      <div style={this.props.style}>
-        <ul>
-         {questionsList.filter(question =>
-          (question.optionOne.votes.indexOf("sarahedo")<=-1)&&
-          (question.optionTwo.votes.indexOf("sarahedo")<=-1)).map(question => <li>{question.author}</li>)}
-        </ul>
-        <Question name={"Nouar"} />     
-      
-      </div>
+      <ListGroup>
+        {console.log("filter = :",this.props.filter)}
+        {this.props.filter === "answered"
+          ? questionsList
+              .filter(
+                (question) =>
+                  question.optionOne.votes.indexOf("sarahedo") > -1 ||
+                  question.optionTwo.votes.indexOf("sarahedo") > -1
+              )
+              .map((question) => (
+                <ListGroup.Item key={question.id}>
+                  <Question question={question} />
+                </ListGroup.Item>
+              ))
+          : questionsList
+              .filter(
+                (question) =>
+                  question.optionOne.votes.indexOf("sarahedo") <= -1 &&
+                  question.optionTwo.votes.indexOf("sarahedo") <= -1
+              )
+              .map((question) => (
+                <ListGroup.Item key={question.id}>
+                  <Question question={question} />
+                </ListGroup.Item>
+              ))}
+      </ListGroup>
     );
   }
 }
+
 
 class Home extends Component {
+  componentDidMount() {
+    if (this.props.authedUser !== null) this.props.dispatch(getInitialData());
+  }
 
-  componentDidMount(){
-    if(this.props.authedUser!==null)
-      this.props.dispatch(getInitialData())
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      filter: "answered",
+    };
+  }
+
+  toggleFilter(e) {
+    console.log("event", e);
+    this.setState({
+      filter: e,
+    });
   }
 
   render() {
-    const {questions,authedUser} = this.props
+    const { questions, authedUser } = this.props;
 
     if (this.props.authedUser === null) return <Redirect to="/Login" />;
 
     return (
-      <Row className="justify-content-center fluid">
-        <Col className="col-sm-12 col-lg-8 ">
-          <CoolTabs
-            tabKey={"1"}
-            style={{ width: 550, height: 500, background: "white" }}
-            activeTabStyle={{ background: "#B777FF", color: "white" }}
-            unActiveTabStyle={{ background: "#DBDBDB", color: "gray" }}
-            activeLeftTabBorderBottomStyle={{
-              background: "#B777FF",
-              height: 4,
-            }}
-            activeRightTabBorderBottomStyle={{
-              background: "#B777FF",
-              height: 4,
-            }}
-            tabsBorderBottomStyle={{ background: "gray", height: 4 }}
-            leftContentStyle={{ background: "#B777FF" }}
-            rightContentStyle={{ background: "#B777FF" }}
-            leftTabTitle={"Answered Questions"}
-            rightTabTitle={"Unanswered Questions"}
-            leftContent={<Content1 questions={questions} authedUser={authedUser} />}
-            rightContent={<Content2 questions={questions} authedUser={authedUser} />}
-            contentTransitionStyle={"transform 0.6s ease-in"}
-            borderTransitionStyle={"all 0.6s ease-in"}
-          />
+      <Container className="justify-content-center fluid">
+        <Col
+          className="text-center col-md-12 col-lg-8 justify-content-center"
+          style={{ margin: "auto" }}
+        >
+          <Nav
+            justify
+            variant="tabs"
+            defaultActiveKey="answered"
+            onSelect={(e) => this.toggleFilter(e)}
+          >
+            <Nav.Item>
+              <Nav.Link eventKey="answered" style={{ color: "#9615a5ed" }}>
+                Answered Questions
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="unanswered" style={{ color: "#9615a5ed" }}>
+                Unanswered Questions
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Row style={{ backgroundColor: "white", margin: "0px 1px 0px 1px" }}>
+            <QuestionList questions={questions} filter={this.state.filter} />
+          </Row>
         </Col>
-      </Row>
+      </Container>
     );
   }
 }
