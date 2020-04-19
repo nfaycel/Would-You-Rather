f5f9334383a6a4ad979e76a54c9f9a733fa53bed
faycel <debian2007.faical@gmail.com>
2020-04-19 15:07:02 +0100

    NotFound page: added styles and a link to home page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import { connect } from 'react-redux'
-
+import React from "react";
+import { connect } from "react-redux";
+import {Link} from "react-router-dom"
 
 const NotFound = () => (
   <div>
-    <h1>Not found </h1>
+    <div id="notfound">
+      <div className="notfound">
+        <div className="notfound-404">
+          <h1>
+            4<span>0</span>4
+          </h1>
+        </div>
+        <h2>Oops!This page could Not Be Found</h2>
+        <Link to="/">Go To HomePage</Link>
+      </div>
+    </div>
   </div>
 );
-function mapStateToProps ({authedUser}) {
+
+function mapStateToProps({ authedUser }) {
   return {
-   authedUser,
- }
+    authedUser,
+  };
 }
-export default connect(mapStateToProps)(NotFound)
+export default connect(mapStateToProps)(NotFound);
